c6b654595e20ea3982bcf409c56bbfd4429c5869
Carlos Mendoza <inhack20@gmail.com>
2014-09-19 15:51:40 -0430

    Definiendo entidades de maestros

---
+++ src/Pequiven/MasterBundle/Entity/ArrangementProgram/CategoryArrangementProgram.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Pequiven\MasterBundle\Entity\ArrangementProgram;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Categoria del programa de gestion
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class CategoryArrangementProgram
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="string", length=255)
+     */
+    private $description;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="status", type="integer")
+     */
+    private $status;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     * @return CategoryArrangementProgram
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string 
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set status
+     *
+     * @param integer $status
+     * @return CategoryArrangementProgram
+     */
+    public function setStatus($status)
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    /**
+     * Get status
+     *
+     * @return integer 
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+}
---
+++ src/Pequiven/MasterBundle/Entity/ArrangementProgram/TypeGoal.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace Pequiven\MasterBundle\Entity\ArrangementProgram;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Tipo de meta
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class TypeGoal
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * Descripcion
+     * @var string
+     *
+     * @ORM\Column(name="description", type="string", length=255)
+     */
+    private $description;
+
+    /**
+     * @var \Pequiven\MasterBundle\Entity\ArrangementProgram\CategoryArrangementProgram
+     *
+     * @ORM\ManyToOne(targetEntity="Pequiven\MasterBundle\Entity\ArrangementProgram\CategoryArrangementProgram")
+     */
+    private $categoryArrangementProgram;
+    
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="status", type="boolean")
+     */
+    private $active;
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     * @return TypeGoal
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string 
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set categoryArrangementProgram
+     *
+     * @param integer $categoryArrangementProgram
+     * @return TypeGoal
+     */
+    public function setCategoryArrangementProgram($categoryArrangementProgram)
+    {
+        $this->categoryArrangementProgram = $categoryArrangementProgram;
+
+        return $this;
+    }
+
+    /**
+     * Get categoryArrangementProgram
+     *
+     * @return integer 
+     */
+    public function getCategoryArrangementProgram()
+    {
+        return $this->categoryArrangementProgram;
+    }
+}
