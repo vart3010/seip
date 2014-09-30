a4a64b071a18231c388c97c11e4798bc19108349
Carlos Mendoza <inhack20@gmail.com>
2014-09-29 22:27:52 -0430

    Añadiendo base de tabla extjs

--- src/Pequiven/ArrangementProgramBundle/Controller/Rest/RestArrangementProgramController.php
+++ src/Pequiven/ArrangementProgramBundle/Controller/Rest/RestArrangementProgramController.php
@@ -8,6 +8,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\Controller\Annotations;
 
 /**
  * Controlador rest de los programas de gestion
@@ -18,8 +19,7 @@ use FOS\RestBundle\Controller\FOSRestController;
 class RestArrangementProgramController extends FOSRestController
 {
     /**
-     * 
-     * @Route("/{id}/goals-details")
+     * @Annotations\Get("/{id}/goals-details.{_format}",name="get_arrangementprogram_rest_restarrangementprogram_putgoalsdetails",requirements={"_format"="html|json|xml"},defaults={"_format"="html"})
      */
     function getGoalsDetailsAction($id) {
         $em = $this->getDoctrine()->getManager();
@@ -29,7 +29,37 @@ class RestArrangementProgramController extends FOSRestController
         if (!$entity) {
             throw $this->createNotFoundException('Unable to find ArrangementProgram entity.');
         }
+        $timelines = $entity->getTimelines();
+        $data = $timelines[0]->getGoals();
         $view = $this->view();
+        $result = array(
+            'data' => $data,
+        );
+        $view->setData($result);
+        $view->getSerializationContext()->setGroups(array('id','api_list','goalDetails'));
+        $view->setTemplate("PequivenArrangementProgramBundle:Rest:ArrangementProgram/form.html.twig");
+        return $view;
+    }
+    
+    /**
+     * @Annotations\Put("/{id}/goals-details.{_format}/{slug}",name="put_arrangementprogram_rest_restarrangementprogram_putgoalsdetails",requirements={"_format"="html|json|xml"},defaults={"_format"="html"})
+     */
+    function putGoalsDetailsAction($id) {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('PequivenArrangementProgramBundle:ArrangementProgram')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find ArrangementProgram entity.');
+        }
+        $timelines = $entity->getTimelines();
+        $data = $timelines[0]->getGoals();
+        $view = $this->view();
+        $result = array(
+            'data' => $data,
+        );
+        $view->setData($result);
+        $view->getSerializationContext()->setGroups(array('id','api_list','goalDetails'));
         $view->setTemplate("PequivenArrangementProgramBundle:Rest:ArrangementProgram/form.html.twig");
         return $view;
     }
