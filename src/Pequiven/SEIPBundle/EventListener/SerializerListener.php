fd8d302c14ba68d8b8a0b8d272cef147eff96762
Carlos Mendoza <inhack20@gmail.com>
2014-10-15 11:11:17 -0430

    Agregando validaciones para los trimestres

--- src/Pequiven/SEIPBundle/EventListener/SerializerListener.php
+++ src/Pequiven/SEIPBundle/EventListener/SerializerListener.php
@@ -101,6 +101,37 @@ class SerializerListener implements EventSubscriberInterface,  ContainerAwareInt
         //Deshabilitar las celdas planeadas cuando se distribuya el 100%
         $disablePlannedOnComplete = true;
         
+        //Habilitar la carga por trimestre
+        $isEnabledLoadByQuarter = true;
+        
+        //Habilitar la carga del primer trimestre (Requiere isEnabledLoadByQuarter)
+        $isEnabledLoadByQuarterFirst = false;
+        //Habilitar la carga de valores planificados del primer trimestre (Requiere isEnabledLoadByQuarterFirst)
+        $isEnabledLoadByQuarterFirstPlanned = true;
+        //Habilitar la carga de valores reales del primer trimestre (Requiere isEnabledLoadByQuarterFirst)
+        $isEnabledLoadByQuarterFirstReal = true;
+        
+        //Habilitar la carga del segundo trimestre (Requiere isEnabledLoadByQuarter)
+        $isEnabledLoadByQuarterSecond = false;
+        //Habilitar la carga de valores planificados del segundo trimestre (Requiere isEnabledLoadByQuarterSecond)
+        $isEnabledLoadByQuarterSecondPlanned = true;
+        //Habilitar la carga de valores reales del segundo trimestre (Requiere isEnabledLoadByQuarterSecond)
+        $isEnabledLoadByQuarterSecondReal = true;
+        
+        //Habilitar la carga del tercer trimestre (Requiere isEnabledLoadByQuarter)
+        $isEnabledLoadByQuarterThird = false;
+        //Habilitar la carga de valores planificados del tercer trimestre (Requiere isEnabledLoadByQuarterThird)
+        $isEnabledLoadByQuarterThirdPlanned = true;
+        //Habilitar la carga de valores reales del tercer trimestre (Requiere isEnabledLoadByQuarterThird)
+        $isEnabledLoadByQuarterThirdReal = true;
+        
+        //Habilitar la carga del cuarto trimestre (Requiere isEnabledLoadByQuarter)
+        $isEnabledLoadByQuarterFourth = false;
+        //Habilitar la carga de valores planificados del cuarto trimestre (Requiere isEnabledLoadByQuarterFourth)
+        $isEnabledLoadByQuarterFourthPlanned = true;
+        //Habilitar la carga de valores reales del cuarto trimestre (Requiere isEnabledLoadByQuarterFourth)
+        $isEnabledLoadByQuarterFourthReal = true;
+        
         $month = $date->format('m');
         
         if($isLoadRealEnabled === false){
@@ -127,17 +158,7 @@ class SerializerListener implements EventSubscriberInterface,  ContainerAwareInt
                 }
             }
         }
-        if($isEnabledEditByPlannedLoad === true){
-            $propertyAccessor = new \Symfony\Component\PropertyAccess\PropertyAccessor();
-            foreach (GoalDetails::getMonthsPlanned() as $planned => $monthNumber) {
-                $value = $propertyAccessor->getValue($object,$planned);
-                $monthReal = GoalDetails::getMonthOfRealByMonth($monthNumber);
-                if($value == '' || $value == '0' || $value === null){
-                    $data[$monthReal]['isEnabled'] = false;
-                }
-            }
-        }
-        
+                
         if($disablePlannedOnComplete === true){
             //Limite de porcentaje que se asigna al planeado
             $limitPlannedPercentaje = 100;
@@ -162,6 +183,102 @@ class SerializerListener implements EventSubscriberInterface,  ContainerAwareInt
                 }
             }
         }
+        
+        if($isEnabledLoadByQuarter === true){
+            $currentCuarter = null;
+            $quarterDef = array(
+                1 => array(1,2,3),
+                2 => array(4,5,6),
+                3 => array(7,8,9),
+                4 => array(10,11,12),
+            );
+            foreach ($quarterDef as $key => $quarter) {
+                foreach ($quarter as $monthQuarter) {
+                    if($monthQuarter == $month){
+                        $currentCuarter = $key;
+                        break;
+                    }
+                }
+                if($currentCuarter !== null){
+                    break;
+                }
+            }
+            $disableQuartes = function ($months,&$data,$currentCuarter) use ($quarterDef) {
+                foreach ($months as $planned => $monthNumber) {
+                    $disable = true;
+                    foreach ($quarterDef[$currentCuarter] as $monthQuarter) {
+                        if($monthNumber == $monthQuarter){
+                            $disable = false;
+                        }
+                    }
+                    if($disable === true){
+                        $data[$planned]['isEnabled'] = false;
+                    }
+                }
+            };
+            
+            $enableQuartes = function($months,&$data,$cuarter) use ($quarterDef) {
+                 foreach ($months as $planned => $monthNumber) {
+                    $disable = true;
+                    foreach ($quarterDef[$cuarter] as $monthQuarter) {
+                        if($monthNumber == $monthQuarter){
+                            $disable = false;
+                        }
+                    }
+                    if($disable === false){
+                        $data[$planned]['isEnabled'] = true;
+                    }
+                }
+            };
+            
+            $disableQuartes(GoalDetails::getMonthsPlanned(),$data,$currentCuarter);
+            $disableQuartes(GoalDetails::getMonthsReal(),$data,$currentCuarter);
+            
+            if($isEnabledLoadByQuarterFirst === true){
+                if($isEnabledLoadByQuarterFirstPlanned === true){
+                    $enableQuartes(GoalDetails::getMonthsPlanned(),$data,1);
+                }
+                if($isEnabledLoadByQuarterFirstReal === true){
+                    $enableQuartes(GoalDetails::getMonthsReal(),$data,1);
+                }
+            }
+            if($isEnabledLoadByQuarterSecond === true){
+                if($isEnabledLoadByQuarterSecondPlanned === true){
+                    $enableQuartes(GoalDetails::getMonthsPlanned(),$data,2);
+                }
+                if($isEnabledLoadByQuarterSecondReal === true){
+                    $enableQuartes(GoalDetails::getMonthsReal(),$data,2);
+                }
+            }
+            if($isEnabledLoadByQuarterThird === true){
+                if($isEnabledLoadByQuarterThirdPlanned === true){
+                    $enableQuartes(GoalDetails::getMonthsPlanned(),$data,3);
+                }
+                if($isEnabledLoadByQuarterThirdReal === true){
+                    $enableQuartes(GoalDetails::getMonthsReal(),$data,3);
+                }
+            }
+            if($isEnabledLoadByQuarterFourth === true){
+                if($isEnabledLoadByQuarterFourthPlanned === true){
+                    $enableQuartes(GoalDetails::getMonthsPlanned(),$data,4);
+                }
+                if($isEnabledLoadByQuarterFourthReal === true){
+                    $enableQuartes(GoalDetails::getMonthsReal(),$data,4);
+                }
+            }
+        }
+        
+        if($isEnabledEditByPlannedLoad === true){
+            $propertyAccessor = new \Symfony\Component\PropertyAccess\PropertyAccessor();
+            foreach (GoalDetails::getMonthsPlanned() as $planned => $monthNumber) {
+                $value = $propertyAccessor->getValue($object,$planned);
+                $monthReal = GoalDetails::getMonthOfRealByMonth($monthNumber);
+                if($value == '' || $value == '0' || $value === null){
+                    $data[$monthReal]['isEnabled'] = false;
+                }
+            }
+        }
+        
         $event->getVisitor()->addData('_data',$data);
         
     }
