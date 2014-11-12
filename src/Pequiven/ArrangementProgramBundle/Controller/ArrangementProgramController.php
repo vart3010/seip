3ca29c13d3dfdef14ef91dc7750a29b3541859a3
Carlos Mendoza <inhack20@gmail.com>
2014-11-12 13:01:47 -0430

    Se limito a 5 los resultados del box. y se corrigio el permiso

--- src/Pequiven/ArrangementProgramBundle/Controller/ArrangementProgramController.php
+++ src/Pequiven/ArrangementProgramBundle/Controller/ArrangementProgramController.php
@@ -703,7 +703,7 @@ class ArrangementProgramController extends SEIPController
     {
         $resource = $this->findOr404($request);
         
-        if(!$this->getArrangementProgramManager()->hasPermissionToUpdate($resource)){
+        if(!$this->getArrangementProgramManager()->hasPermissionToAddComment($resource)){
             throw $this->createAccessDeniedHttpException();
         }
         $view = $this->view();
