fcce30f8623070af2503f6426218a50d6686604f
Carlos Mendoza <inhack20@gmail.com>
2014-11-14 15:21:16 -0430

    Definiendo entidad de variables

---
+++ src/Pequiven/MasterBundle/Entity/Formula/Variable.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace Pequiven\MasterBundle\Entity\Formula;
+
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
+
+/**
+ * Variable
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Variable
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="string", length=255)
+     */
+    private $description;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=100)
+     */
+    private $name;
+
+    /**
+     * @var \DateTime
+     * @Gedmo\Timestampable(on="create")
+     * @ORM\Column(name="created_at", type="datetime")
+     */
+    private $createdAt;
+
+    /**
+     * @var \DateTime
+     * @Gedmo\Timestampable(on="update")
+     * @ORM\Column(name="updated_at", type="datetime")
+     */
+    private $updatedAt;
+
+     /**
+     * Usuario que creo la variable
+     * @var \Pequiven\SEIPBundle\Entity\User
+     * @ORM\ManyToOne(targetEntity="\Pequiven\SEIPBundle\Entity\User")
+     * @ORM\JoinColumn(referencedColumnName="id",nullable=false)
+     */
+    private $createdBy;
+    
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     * @return Variable
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string 
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Variable
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+}
