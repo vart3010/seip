61544cd19d53baaa7c39986dc48f493f1ebbf589
Carlos Mendoza <inhack20@gmail.com>
2014-12-10 14:05:41 -0430

    Agregando sql de actualizacion

--- app/update/1.42.sql
+++ app/update/1.42.sql
@@ -1,3 +1,7 @@
+ALTER TABLE seip_c_gerencia ADD gerenciaGroup_id INT DEFAULT NULL;
+ALTER TABLE seip_c_gerencia ADD CONSTRAINT FK_12A7F17C4DD84A60 FOREIGN KEY (gerenciaGroup_id) REFERENCES seip_gerencia_group (id);
+CREATE INDEX IDX_12A7F17C4DD84A60 ON seip_c_gerencia (gerenciaGroup_id);
+
 UPDATE `seip`.`seip_c_gerencia` SET `gerenciaGroup_id` = '3' WHERE `seip_c_gerencia`.`id` = 7;
 UPDATE `seip`.`seip_c_gerencia` SET `gerenciaGroup_id` = '3' WHERE `seip_c_gerencia`.`id` = 1;
 UPDATE `seip`.`seip_c_gerencia` SET `gerenciaGroup_id` = '3' WHERE `seip_c_gerencia`.`id` = 9;
