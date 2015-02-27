250bf146df5ac26d8a5ad7b3ef6cb2d864642914
Matias <matei249@gmail.com>
2015-02-27 15:17:34 -0430

    Inicio de resultados estratégicos

--- src/Pequiven/MasterBundle/Controller/LineStrategicController.php
+++ src/Pequiven/MasterBundle/Controller/LineStrategicController.php
@@ -25,7 +25,7 @@ class LineStrategicController extends SEIPController {
      * @return type
      */
     public function viewDashboardAction(Request $request){
-        $boxRender = $this->get('tecnocreaciones_box.render');        
+        $boxRender = $this->get('tecnocreaciones_box.render');
         
         $view = $this
                 ->view()
@@ -38,5 +38,133 @@ class LineStrategicController extends SEIPController {
         return $this->handleView($view);
     }
     
+    public function showAction(Request $request) {
+        
+        $resource = $this->findOr404($request);
+        
+        $resultIndicator = $resultArrangementProgram = $resultObjetives = array();
+        $objetives = $resource->getObjetives();
+        $showResultObjetives = false;
+        
+        $caption = $this->trans('result.captionObjetiveStrategic',array(),'PequivenSEIPBundle');
+        $subCaption = $this->getPeriodService()->getPeriodActive()->getDescription();
+        $data = array(
+            'dataSource' => array(
+                'chart' => array(
+                    'caption' => $caption,
+                    'subCaption' => $subCaption,
+                ),
+                'categories' => array(
+                    'category' => array(),
+                ),
+                'dataset' => array(),
+            ),
+        );
+        //Configuramos el alto del gráfico
+        $totalObjects = count($objetives);
+        $heightChart = ($totalObjects * 30) + 150;
+        
+        //Data del gráfico
+        foreach($objetives as $objetive){
+            $refObjetive = $objetive->getRef();
+            $flagResultIndicator = $flagResultObjetives = false;
+            $categories[] = array('label' => $refObjetive);
+            foreach($objetive->getResults() as $result){
+                $urlObjetive =  $this->generateUrl('objetiveStrategic_show', array('id' => $objetive->getId()));
+                $totalIndicator = 0.0;
+                $totalObjetives = 0.0;
+                $flagResultIndicatorInternal = false;
+                $flagResultObjetivesInternal = false;
+
+                if($result->getTypeResult() == \Pequiven\SEIPBundle\Model\Result\Result::TYPE_RESULT_INDICATOR){
+                    $totalIndicator += $result->getResultWithWeight();
+                    $flagResultIndicator = $flagResultIndicatorInternal = true;
+                }
+                if($result->getTypeResult() == \Pequiven\SEIPBundle\Model\Result\Result::TYPE_RESULT_OBJECTIVE){
+                    $totalObjetives+= $result->getResultWithWeight();
+                    $flagResultObjetives = $flagResultObjetivesInternal = true;
+                }
+                if($result->getTypeResult() == \Pequiven\SEIPBundle\Model\Result\Result::TYPE_RESULT_OF_RESULT){
+                    foreach ($result->getChildrens() as $child) {
+                        if($child->getTypeResult() == \Pequiven\SEIPBundle\Model\Result\Result::TYPE_RESULT_INDICATOR){
+                            $totalIndicator += ($child->getResultWithWeight()*$result->getWeight())/100;
+                            $flagResultIndicator = $flagResultIndicatorInternal = true;
+                        }elseif($child->getTypeResult() == \Pequiven\SEIPBundle\Model\Result\Result::TYPE_RESULT_OBJECTIVE){
+                            $totalObjetives+= ($child->getResultWithWeight()*$result->getWeight())/100;
+                            $flagResultObjetives = $flagResultObjetivesInternal = true;
+                        }
+                    }
+                }
+
+                if($flagResultIndicatorInternal === true){
+                    $resultIndicator[] = array('value' => bcadd($totalIndicator,'0',2),'link' => $urlObjetive);
+                }
+                if($flagResultObjetivesInternal === true){
+                    $resultObjetives[] = array('value' => bcadd($totalObjetives,'0',2),'link' => $urlObjetive, 'bgColor' => '');
+                }
+            }
+
+            if(!$flagResultIndicator){
+                $resultIndicator[] = array('value' => bcadd(0,'0',2));
+
+            }
+            if(!$flagResultObjetives){
+                $resultObjetives[] = array('value' => bcadd(0,'0',2));
+            }
+        }
+        if(count($resultIndicator) > 0){
+            $data['dataSource']['dataset'][] = array(
+                    'seriesname' => $this->trans('chart.result.objetiveOperative.seriesNamePlan1'),
+                    'data' => $resultIndicator,
+                );
+        }
+        if(count($resultObjetives) > 0){
+            $data['dataSource']['dataset'][] = array(
+                'seriesname' => $this->trans('chart.result.objetiveOperative.seriesNamePlan3'),
+                'data' => $resultObjetives,
+            );
+        }
+        
+        $data['dataSource']['categories']['category'] = $categories;
+        
+        $resultService = $this->getResultService();
+        
+        $boxRender = $this->get('tecnocreaciones_box.render');
+        
+        $dataArray =  array(
+            $this->config->getResourceName() => $resource,
+            'object' => $objetives,
+            'heightChart' => $heightChart,
+            'data' => $data,
+            'resultService' => $resultService,
+            'boxRender' => $boxRender
+        );
+        
+        $view = $this
+                ->view()
+                ->setTemplate($this->config->getTemplate('show.html'))
+                ->setData($dataArray)
+                ;
+        
+        return $this->handleView($view);
+    }
+    
+    /**
+     * 
+     * @return \Pequiven\SEIPBundle\Service\PeriodService
+     */
+    protected function getPeriodService()
+    {
+        return $this->container->get('pequiven_arrangement_program.service.period');
+    }
+    
+    /**
+     * 
+     * @return \Pequiven\SEIPBundle\Service\ResultService
+     */
+    protected function getResultService()
+    {
+        return $this->container->get('seip.service.result');
+    }
 }
 
