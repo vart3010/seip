e966c72c206a32859705111ccb858d611334980e
Carlos Mendoza <inhack20@gmail.com>
2015-05-21 23:18:59 -0430

    Agreganto boton para calcular los servicios

--- src/Pequiven/SEIPBundle/Controller/DataLoad/PlantReportController.php
+++ src/Pequiven/SEIPBundle/Controller/DataLoad/PlantReportController.php
@@ -33,4 +33,59 @@ class PlantReportController extends SEIPController
         }
         return $entity;
     }
+    
+    public function runAction(Request $request)
+    {
+        $resource = $this->findOr404($request);
+        
+        $plantStopPlanningsByMonths = $resource->getPlantStopPlanningSortByMonth();
+        $consumerPlanningServices = $resource->getConsumerPlanningServices();
+        $propertyAccessor = \Symfony\Component\PropertyAccess\PropertyAccess::createPropertyAccessor();
+        
+        foreach ($consumerPlanningServices as $consumerPlanningService) {
+            $details = $consumerPlanningService->getDetails();
+            foreach ($details as $detail) {
+                $daysStopsArray = array();
+                $month = $detail->getMonth();
+                if(isset($plantStopPlanningsByMonths[$month])){
+                    $daysStops = $plantStopPlanningsByMonths[$month];
+                    foreach ($daysStops as $daysStop) {
+                        $daysStopsArray[] = $daysStop->getNroDay();
+                    }
+                }
+                $ranges = $detail->getRanges();
+                foreach ($ranges as $range) {
+                    $monthBudget = $detail->getMonthBudget();
+                    $dateFrom = $range->getDateFrom();
+                    $dateEnd = $range->getDateEnd();
+
+                    $dayFrom = $dateFrom->format("d");
+                    $dayEnd = $dateEnd->format("d");
+                    $type = $range->getType();
+                    $originalValue = $range->getValue();
+                    $value = 0;
+
+                    if($type === \Pequiven\SEIPBundle\Model\DataLoad\RawMaterial\Range::TYPE_FIXED_VALUE){
+                        $value = $originalValue;
+                    }else if($type === \Pequiven\SEIPBundle\Model\DataLoad\RawMaterial\Range::TYPE_PERCENTAGE_BUDGET){
+                        $value = ($monthBudget / 100) * $originalValue;
+                    }
+
+                    for($day = $dayFrom; $day < $dayEnd; $day++){
+                        $dayInt = (int)$day;
+                        $propertyPath = sprintf("day%sPlan",$dayInt);
+                        if(in_array($dayInt,$daysStopsArray)){
+                            $propertyAccessor->setValue($details, $propertyPath, 0);
+                            continue;
+                        }
+                        $propertyAccessor->setValue($detail, $propertyPath, $value);
+                        $this->save($detail);
+                    }
+                }
+            }
+
+        }
+        $this->flush();
+        return $this->redirectHandler->redirectTo($resource);
+    }
 }
