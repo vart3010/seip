1ddbb71e2eb49e7e23d5a55351366ffde47d70b5
Carlos Mendoza <inhack20@gmail.com>
2015-05-28 17:52:37 -0430

    Auto persist

--- src/Pequiven/SEIPBundle/Entity/DataLoad/Production/UnrealizedProduction.php
+++ src/Pequiven/SEIPBundle/Entity/DataLoad/Production/UnrealizedProduction.php
@@ -43,210 +43,210 @@ class UnrealizedProduction extends DetailValue
     /**
      * Dia 2 (Detalles de PNR)
      * @var UnrealizedProductionDay
-     * @ORM\OneToOne(targetEntity="Pequiven\SEIPBundle\Entity\DataLoad\Production\UnrealizedProductionDay")
+     * @ORM\OneToOne(targetEntity="Pequiven\SEIPBundle\Entity\DataLoad\Production\UnrealizedProductionDay",cascade={"persist","remove"})
      */
     protected $day2Details;
     
     /**
      * Dia 3 (Detalles de PNR)
      * @var UnrealizedProductionDay
-     * @ORM\OneToOne(targetEntity="Pequiven\SEIPBundle\Entity\DataLoad\Production\UnrealizedProductionDay")
+     * @ORM\OneToOne(targetEntity="Pequiven\SEIPBundle\Entity\DataLoad\Production\UnrealizedProductionDay",cascade={"persist","remove"})
      */
     protected $day3Details;
     
     /**
      * Dia 4 (Detalles de PNR)
      * @var UnrealizedProductionDay
-     * @ORM\OneToOne(targetEntity="Pequiven\SEIPBundle\Entity\DataLoad\Production\UnrealizedProductionDay")
+     * @ORM\OneToOne(targetEntity="Pequiven\SEIPBundle\Entity\DataLoad\Production\UnrealizedProductionDay",cascade={"persist","remove"})
      */
     protected $day4Details;
     
     /**
      * Dia 5 (Detalles de PNR)
      * @var UnrealizedProductionDay
-     * @ORM\OneToOne(targetEntity="Pequiven\SEIPBundle\Entity\DataLoad\Production\UnrealizedProductionDay")
+     * @ORM\OneToOne(targetEntity="Pequiven\SEIPBundle\Entity\DataLoad\Production\UnrealizedProductionDay",cascade={"persist","remove"})
      */
     protected $day5Details;
     
     /**
      * Dia 6 (Detalles de PNR)
      * @var UnrealizedProductionDay
-     * @ORM\OneToOne(targetEntity="Pequiven\SEIPBundle\Entity\DataLoad\Production\UnrealizedProductionDay")
+     * @ORM\OneToOne(targetEntity="Pequiven\SEIPBundle\Entity\DataLoad\Production\UnrealizedProductionDay",cascade={"persist","remove"})
      */
     protected $day6Details;
         
     /**
      * Dia 7 (Detalles de PNR)
      * @var UnrealizedProductionDay
-     * @ORM\OneToOne(targetEntity="Pequiven\SEIPBundle\Entity\DataLoad\Production\UnrealizedProductionDay")
+     * @ORM\OneToOne(targetEntity="Pequiven\SEIPBundle\Entity\DataLoad\Production\UnrealizedProductionDay",cascade={"persist","remove"})
      */
     protected $day7Details;
     
     /**
      * Dia 8 (Detalles de PNR)
      * @var UnrealizedProductionDay
-     * @ORM\OneToOne(targetEntity="Pequiven\SEIPBundle\Entity\DataLoad\Production\UnrealizedProductionDay")
+     * @ORM\OneToOne(targetEntity="Pequiven\SEIPBundle\Entity\DataLoad\Production\UnrealizedProductionDay",cascade={"persist","remove"})
      */
     protected $day8Details;
     
     /**
      * Dia 9 (Detalles de PNR)
      * @var UnrealizedProductionDay
-     * @ORM\OneToOne(targetEntity="Pequiven\SEIPBundle\Entity\DataLoad\Production\UnrealizedProductionDay")
+     * @ORM\OneToOne(targetEntity="Pequiven\SEIPBundle\Entity\DataLoad\Production\UnrealizedProductionDay",cascade={"persist","remove"})
      */
     protected $day9Details;
     
     /**
      * Dia 10 (Detalles de PNR)
      * @var UnrealizedProductionDay
-     * @ORM\OneToOne(targetEntity="Pequiven\SEIPBundle\Entity\DataLoad\Production\UnrealizedProductionDay")
+     * @ORM\OneToOne(targetEntity="Pequiven\SEIPBundle\Entity\DataLoad\Production\UnrealizedProductionDay",cascade={"persist","remove"})
      */
     protected $day10Details;
     
     /**
      * Dia 11 (Detalles de PNR)
      * @var UnrealizedProductionDay
-     * @ORM\OneToOne(targetEntity="Pequiven\SEIPBundle\Entity\DataLoad\Production\UnrealizedProductionDay")
+     * @ORM\OneToOne(targetEntity="Pequiven\SEIPBundle\Entity\DataLoad\Production\UnrealizedProductionDay",cascade={"persist","remove"})
      */
     protected $day11Details;
     
     /**
      * Dia 12 (Detalles de PNR)
      * @var UnrealizedProductionDay
-     * @ORM\OneToOne(targetEntity="Pequiven\SEIPBundle\Entity\DataLoad\Production\UnrealizedProductionDay")
+     * @ORM\OneToOne(targetEntity="Pequiven\SEIPBundle\Entity\DataLoad\Production\UnrealizedProductionDay",cascade={"persist","remove"})
      */
     protected $day12Details;
     
     /**
      * Dia 13 (Detalles de PNR)
      * @var UnrealizedProductionDay
-     * @ORM\OneToOne(targetEntity="Pequiven\SEIPBundle\Entity\DataLoad\Production\UnrealizedProductionDay")
+     * @ORM\OneToOne(targetEntity="Pequiven\SEIPBundle\Entity\DataLoad\Production\UnrealizedProductionDay",cascade={"persist","remove"})
      */
     protected $day13Details;
     
     /**
      * Dia 14 (Detalles de PNR)
      * @var UnrealizedProductionDay
-     * @ORM\OneToOne(targetEntity="Pequiven\SEIPBundle\Entity\DataLoad\Production\UnrealizedProductionDay")
+     * @ORM\OneToOne(targetEntity="Pequiven\SEIPBundle\Entity\DataLoad\Production\UnrealizedProductionDay",cascade={"persist","remove"})
      */
     protected $day14Details;
     
     /**
      * Dia 15 (Detalles de PNR)
      * @var UnrealizedProductionDay
-     * @ORM\OneToOne(targetEntity="Pequiven\SEIPBundle\Entity\DataLoad\Production\UnrealizedProductionDay")
+     * @ORM\OneToOne(targetEntity="Pequiven\SEIPBundle\Entity\DataLoad\Production\UnrealizedProductionDay",cascade={"persist","remove"})
      */
     protected $day15Details;
     
     /**
      * Dia 16 (Detalles de PNR)
      * @var UnrealizedProductionDay
-     * @ORM\OneToOne(targetEntity="Pequiven\SEIPBundle\Entity\DataLoad\Production\UnrealizedProductionDay")
+     * @ORM\OneToOne(targetEntity="Pequiven\SEIPBundle\Entity\DataLoad\Production\UnrealizedProductionDay",cascade={"persist","remove"})
      */
     protected $day16Details;
     
     /**
      * Dia 17 (Detalles de PNR)
      * @var UnrealizedProductionDay
-     * @ORM\OneToOne(targetEntity="Pequiven\SEIPBundle\Entity\DataLoad\Production\UnrealizedProductionDay")
+     * @ORM\OneToOne(targetEntity="Pequiven\SEIPBundle\Entity\DataLoad\Production\UnrealizedProductionDay",cascade={"persist","remove"})
      */
     protected $day17Details;
     
     /**
      * Dia 18 (Detalles de PNR)
      * @var UnrealizedProductionDay
-     * @ORM\OneToOne(targetEntity="Pequiven\SEIPBundle\Entity\DataLoad\Production\UnrealizedProductionDay")
+     * @ORM\OneToOne(targetEntity="Pequiven\SEIPBundle\Entity\DataLoad\Production\UnrealizedProductionDay",cascade={"persist","remove"})
      */
     protected $day18Details;
     
     /**
      * Dia 19 (Detalles de PNR)
      * @var UnrealizedProductionDay
-     * @ORM\OneToOne(targetEntity="Pequiven\SEIPBundle\Entity\DataLoad\Production\UnrealizedProductionDay")
+     * @ORM\OneToOne(targetEntity="Pequiven\SEIPBundle\Entity\DataLoad\Production\UnrealizedProductionDay",cascade={"persist","remove"})
      */
     protected $day19Details;
     
     /**
      * Dia 20 (Detalles de PNR)
      * @var UnrealizedProductionDay
-     * @ORM\OneToOne(targetEntity="Pequiven\SEIPBundle\Entity\DataLoad\Production\UnrealizedProductionDay")
+     * @ORM\OneToOne(targetEntity="Pequiven\SEIPBundle\Entity\DataLoad\Production\UnrealizedProductionDay",cascade={"persist","remove"})
      */
     protected $day20Details;
     
     /**
      * Dia 21 (Detalles de PNR)
      * @var UnrealizedProductionDay
-     * @ORM\OneToOne(targetEntity="Pequiven\SEIPBundle\Entity\DataLoad\Production\UnrealizedProductionDay")
+     * @ORM\OneToOne(targetEntity="Pequiven\SEIPBundle\Entity\DataLoad\Production\UnrealizedProductionDay",cascade={"persist","remove"})
      */
     protected $day21Details;
     
     /**
      * Dia 22 (Detalles de PNR)
      * @var UnrealizedProductionDay
-     * @ORM\OneToOne(targetEntity="Pequiven\SEIPBundle\Entity\DataLoad\Production\UnrealizedProductionDay")
+     * @ORM\OneToOne(targetEntity="Pequiven\SEIPBundle\Entity\DataLoad\Production\UnrealizedProductionDay",cascade={"persist","remove"})
      */
     protected $day22Details;
     
     /**
      * Dia 23 (Detalles de PNR)
      * @var UnrealizedProductionDay
-     * @ORM\OneToOne(targetEntity="Pequiven\SEIPBundle\Entity\DataLoad\Production\UnrealizedProductionDay")
+     * @ORM\OneToOne(targetEntity="Pequiven\SEIPBundle\Entity\DataLoad\Production\UnrealizedProductionDay",cascade={"persist","remove"})
      */
     protected $day23Details;
     
     /**
      * Dia 24 (Detalles de PNR)
      * @var UnrealizedProductionDay
-     * @ORM\OneToOne(targetEntity="Pequiven\SEIPBundle\Entity\DataLoad\Production\UnrealizedProductionDay")
+     * @ORM\OneToOne(targetEntity="Pequiven\SEIPBundle\Entity\DataLoad\Production\UnrealizedProductionDay",cascade={"persist","remove"})
      */
     protected $day24Details;
     
     /**
      * Dia 25 (Detalles de PNR)
      * @var UnrealizedProductionDay
-     * @ORM\OneToOne(targetEntity="Pequiven\SEIPBundle\Entity\DataLoad\Production\UnrealizedProductionDay")
+     * @ORM\OneToOne(targetEntity="Pequiven\SEIPBundle\Entity\DataLoad\Production\UnrealizedProductionDay",cascade={"persist","remove"})
      */
     protected $day25Details;
     
     /**
      * Dia 26 (Detalles de PNR)
      * @var UnrealizedProductionDay
-     * @ORM\OneToOne(targetEntity="Pequiven\SEIPBundle\Entity\DataLoad\Production\UnrealizedProductionDay")
+     * @ORM\OneToOne(targetEntity="Pequiven\SEIPBundle\Entity\DataLoad\Production\UnrealizedProductionDay",cascade={"persist","remove"})
      */
     protected $day26Details;
     
     /**
      * Dia 27 (Detalles de PNR)
      * @var UnrealizedProductionDay
-     * @ORM\OneToOne(targetEntity="Pequiven\SEIPBundle\Entity\DataLoad\Production\UnrealizedProductionDay")
+     * @ORM\OneToOne(targetEntity="Pequiven\SEIPBundle\Entity\DataLoad\Production\UnrealizedProductionDay",cascade={"persist","remove"})
      */
     protected $day27Details;
     
     /**
      * Dia 28 (Detalles de PNR)
      * @var UnrealizedProductionDay
-     * @ORM\OneToOne(targetEntity="Pequiven\SEIPBundle\Entity\DataLoad\Production\UnrealizedProductionDay")
+     * @ORM\OneToOne(targetEntity="Pequiven\SEIPBundle\Entity\DataLoad\Production\UnrealizedProductionDay",cascade={"persist","remove"})
      */
     protected $day28Details;
     
     /**
      * Dia 29 (Detalles de PNR)
      * @var UnrealizedProductionDay
-     * @ORM\OneToOne(targetEntity="Pequiven\SEIPBundle\Entity\DataLoad\Production\UnrealizedProductionDay")
+     * @ORM\OneToOne(targetEntity="Pequiven\SEIPBundle\Entity\DataLoad\Production\UnrealizedProductionDay",cascade={"persist","remove"})
      */
     protected $day29Details;
     
     /**
      * Dia 30 (Detalles de PNR)
      * @var UnrealizedProductionDay
-     * @ORM\OneToOne(targetEntity="Pequiven\SEIPBundle\Entity\DataLoad\Production\UnrealizedProductionDay")
+     * @ORM\OneToOne(targetEntity="Pequiven\SEIPBundle\Entity\DataLoad\Production\UnrealizedProductionDay",cascade={"persist","remove"})
      */
     protected $day30Details;
     
     /**
      * Dia 31 (Detalles de PNR)
      * @var UnrealizedProductionDay
-     * @ORM\OneToOne(targetEntity="Pequiven\SEIPBundle\Entity\DataLoad\Production\UnrealizedProductionDay")
+     * @ORM\OneToOne(targetEntity="Pequiven\SEIPBundle\Entity\DataLoad\Production\UnrealizedProductionDay",cascade={"persist","remove"})
      */
     protected $day31Details;
     
