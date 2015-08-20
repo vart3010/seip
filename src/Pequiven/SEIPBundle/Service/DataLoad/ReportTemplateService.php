02b1f00a0eb52c246a9680d60bd7ac65c61fc285
Matias <matei249@gmail.com>
2015-08-20 07:13:08 -0430

    Pantalla negra para el monitor

--- src/Pequiven/SEIPBundle/Service/DataLoad/ReportTemplateService.php
+++ src/Pequiven/SEIPBundle/Service/DataLoad/ReportTemplateService.php
@@ -424,7 +424,7 @@ class ReportTemplateService implements ContainerAwareInterface {
                 //Comparamos si todos los productReports estan notificados en su totalidad
                 if($contProductReports == $dataSerialized[$reportTemplate->getId()]['contNotificationTotal']){
                     $dataSerialized[$reportTemplate->getId()]['color'] = '#0EED59';
-                } elseif($contProductReports != $dataSerialized[$reportTemplate->getId()]['contNotificationTotal'] && !$dataSerialized[$reportTemplate->getId()]['flagNotificationHalf']){
+                } elseif($dataSerialized[$reportTemplate->getId()]['contNotificationTotal'] > 0 && !$dataSerialized[$reportTemplate->getId()]['flagNotificationHalf']){
                     $dataSerialized[$reportTemplate->getId()]['color'] = '#E5E752';
                 } elseif($dataSerialized[$reportTemplate->getId()]['flagNotificationHalf']){
                     $dataSerialized[$reportTemplate->getId()]['color'] = '#E5E752';
