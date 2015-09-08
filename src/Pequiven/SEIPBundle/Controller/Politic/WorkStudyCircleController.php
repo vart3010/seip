a5fab6f656cfcfc2da7640517918291708d7dd3d
victor tortolero <vart10.30@gmail.com>
2015-09-08 15:28:48 -0430

    plantilla de worsStudyCircle

--- src/Pequiven/SEIPBundle/Controller/Politic/WorkStudyCircleController.php
+++ src/Pequiven/SEIPBundle/Controller/Politic/WorkStudyCircleController.php
@@ -34,7 +34,7 @@ class WorkStudyCircleController extends SEIPController {
         //$period = $this->getPeriodService();
         $em->getConnection()->beginTransaction();
         if ($form->isSubmitted() && $form->isValid()) {
-            
+
             $data = $form->getData();
 //            var_dump($data);
 //            die();
@@ -42,14 +42,14 @@ class WorkStudyCircleController extends SEIPController {
             $workStudyCircle->setCreatedBy($user);
             $workStudyCircle->setPeriod($period = $this->getPeriodService()->getPeriodActive());
             //die("Formulario enviado correctamente");
-            
+
             $em->persist($workStudyCircle);
             $em->flush();
-            
+
             $user->setCellphone($request->get("userType_data")["cellphone"]);
             $user->setIndentification($request->get("userType_data")["indentification"]);
             $user->setExt($request->get("userType_data")["ext"]);
-            
+
             try {
                 $em->flush();
                 $em->getConnection()->commit();
@@ -57,27 +57,28 @@ class WorkStudyCircleController extends SEIPController {
                 $em->getConnection()->rollback();
                 throw $e;
             }
-            
+
             $workStudyCircle = $em->getRepository('PequivenSEIPBundle:Politic\WorkStudyCircle')->findOneBy(array('createdBy' => $user->getId()));
-            $this->addWorkStudyCircleToUser($workStudyCircle,$request->get("workStudyCircle_data")["userWorkerId"]);
-            
+            $this->addWorkStudyCircleToUser($workStudyCircle, $request->get("workStudyCircle_data")["userWorkerId"]);
+
             $this->get('session')->getFlashBag()->add('success', 'Círculo de Estudio guardado correctamente');
-            return $this->redirect($this->generateUrl('pequiven_seip_default_index'));
+            //return $this->redirect($this->generateUrl('pequiven_seip_default_index'));
+            return $this->redirect($this->generateUrl('pequiven_work_study_circle_show', array("id" => $workStudyCircle->getId())));
 
             //return $this->redirect($this->generateUrl('saci_people_list', array('id' => $people->getId())));
         }
-        
+
         return $this->render('PequivenSEIPBundle:Politic:WorkStudyCircle\create.html.twig', array(
                     'form' => $form->createView(),
                     'user' => $user,
                     'form_user' => $formUser->createView()
         ));
     }
-    
-    public function addWorkStudyCircleToUser(WorkStudyCircle $workStudyCircle, $members = array()){
+
+    public function addWorkStudyCircleToUser(WorkStudyCircle $workStudyCircle, $members = array()) {
         $em = $this->getDoctrine()->getManager();
         $em->getConnection()->beginTransaction();
-        foreach($members as $member){
+        foreach ($members as $member) {
             $user = $em->getRepository('PequivenSEIPBundle:User')->findOneBy(array('id' => $member));
             $user->setWorkStudyCircle($workStudyCircle);
             $em->persist($user);
@@ -92,6 +93,15 @@ class WorkStudyCircleController extends SEIPController {
         }
 
         return true;
+    }
+
+    public function showAction(Request $request) {
+        $em = $this->getDoctrine()->getManager();
+        $workStudyCircle = $em->getRepository('PequivenSEIPBundle:Politic\WorkStudyCircle')->findOneBy(array('id' => $request->get("id")));
+
+        return $this->render('PequivenSEIPBundle:Politic:WorkStudyCircle\show.html.twig', array(
+                    'workStudyCircle' => $workStudyCircle
+        ));
     }
 
     protected function getPeriodService() {
