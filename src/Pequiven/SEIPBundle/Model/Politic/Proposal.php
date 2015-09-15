e7e5783b4f7dfbc3b27a6bf29b1f3bbe9b13e8a6
Matias <matei249@gmail.com>
2015-09-15 09:07:21 -0430

    Creación de Propuestas

---
+++ src/Pequiven/SEIPBundle/Model/Politic/Proposal.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Pequiven\SEIPBundle\Model\Politic;
+
+use Doctrine\ORM\Mapping as ORM;
+
+use Pequiven\SEIPBundle\Model\BaseModel;
+
+/**
+ * Modelo de las propuestas del círculo de estudio de trabajo
+ *
+ */
+abstract class Proposal  {
+    
+}
