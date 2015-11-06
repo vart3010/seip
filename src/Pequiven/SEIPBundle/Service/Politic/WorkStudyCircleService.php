1c69d22dd0d17190ef939747274c8f53dde72fec
Matias <matei249@gmail.com>
2015-11-06 08:08:29 -0430

    Arreglo 112

--- src/Pequiven/SEIPBundle/Service/Politic/WorkStudyCircleService.php
+++ src/Pequiven/SEIPBundle/Service/Politic/WorkStudyCircleService.php
@@ -631,22 +631,6 @@ class WorkStudyCircleService implements ContainerAwareInterface {
     
     /**
      * 
-     * @param WorkStudyCircle $workStudyCircle
-     * @return boolean
-     */
-    public function isAllowToAddProposals(WorkStudyCircle $workStudyCircle){
-        $valid = false;
-        $user = $this->getUser();
-        
-        if(($this->getSecurityContext()->isGranted(array('ROLE_SEIP_PROPOSAL_ADD')) || $workStudyCircle->getCoordinator()->getId() == $user->getId()) && $workStudyCircle->getPhase() == WorkStudyCircle::PHASE_THREE){
-            $valid = true;
-        }
-        
-        return $valid;
-    }
-    
-    /**
-     * 
      * @return \Symfony\Component\Security\Core\SecurityContext
      * @throws \LogicException
      */
