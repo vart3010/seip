299fc2e3ad22a7d4cd1931a24e82cec34ba7ffd5
Matias <matei249@gmail.com>
2015-11-08 10:48:14 -0430

    Cambios para los filtros en la lista de los CUTL

--- src/Pequiven/SEIPBundle/Repository/Sip/Cutl/CutlRepository.php
+++ src/Pequiven/SEIPBundle/Repository/Sip/Cutl/CutlRepository.php
@@ -22,6 +22,28 @@ class CutlRepository extends EntityRepository {
     public function createPaginatorByCutl(array $criteria = null, array $orderBy = null) {
         return $this->createPaginator($criteria, $orderBy);
     }
+    
+    protected function applyCriteria(\Doctrine\ORM\QueryBuilder $queryBuilder, array $criteria = null) {
+        $criteria = new \Doctrine\Common\Collections\ArrayCollection($criteria);
+        
+        if (($nombreCentro = $criteria->remove('centro'))) {
+            $queryBuilder->andWhere($queryBuilder->expr()->like('ct.centro', "'%".$nombreCentro."%'"));
+        }
+        
+        if (($codigoCentro = $criteria->remove('codigoCentro'))) {
+            $queryBuilder->andWhere($queryBuilder->expr()->like('ct.codigoCentro', "'%".$codigoCentro."%'"));
+        }
+        
+        if (($nombrePersona = $criteria->remove('nombre'))) {
+            $queryBuilder->andWhere($queryBuilder->expr()->like('ct.nombre', "'%".$nombrePersona."%'"));
+        }
+        
+        if (($cedulaPersona = $criteria->remove('cedula'))) {
+            $queryBuilder->andWhere($queryBuilder->expr()->like('ct.cedula', "'%".$cedulaPersona."%'"));
+        }
+        
+        parent::applyCriteria($queryBuilder, $criteria->toArray());
+    }
    
     protected function getAlias() {
         return "ct";
