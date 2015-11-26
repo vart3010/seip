18f1637a563a90b84cb1130a49f3b527f9c29c13
Gilberto <glavrjk@gmail.com>
2015-11-26 09:38:54 -0430

    Reporte Asistencia y Status SIP

--- src/Pequiven/SEIPBundle/Controller/Sip/ReportSIPController.php
+++ src/Pequiven/SEIPBundle/Controller/Sip/ReportSIPController.php
@@ -23,7 +23,7 @@ class ReportSIPController extends SEIPController {
         ));
     }
 
-    public function exportAssistAction(Request $request) {
+    public function AssistAction(Request $request) {
 
         $em = $this->getDoctrine()->getManager();
 
@@ -35,29 +35,6 @@ class ReportSIPController extends SEIPController {
         if ($form->isSubmitted()) {
             $date = $request->get("sip_report")["date"];
             $result = $this->get('pequiven.repository.report')->getAsist($date);
-
-            //Formato para todo el documento
-//            $styleArrayBordersContent = array(
-//                'borders' => array(
-//                    'allborders' => array(
-//                        'style' => \PHPExcel_Style_Border::BORDER_THIN
-//                    )
-//                ),
-//                'font' => array(
-//                ),
-//                'alignment' => array(
-//                    'horizontal' => \PHPExcel_Style_Alignment::HORIZONTAL_JUSTIFY,
-//                    'vertical' => \PHPExcel_Style_Alignment::VERTICAL_TOP,
-//                    'wrap' => true
-//                )
-//            );
-//
-//            //Formato en negrita
-//            $styleArray = array(
-//                'font' => array(
-//                    'bold' => true
-//                )
-//            );
 
             $path = $this->get('kernel')->locateResource('@PequivenSEIPBundle/Resources/Skeleton/Sip/Asistencia.xlsx');
             $now = new \DateTime();
@@ -72,31 +49,33 @@ class ReportSIPController extends SEIPController {
             ;
             $objPHPExcel
                     ->setActiveSheetIndex(0);
-            
-            $activeSheet = $objPHPExcel->getActiveSheet();
-            $row = $iniFile = 4; //Fila Inicial del skeleton
-            $contResult = count($result); //Contador de resultados totales
-            
-            foreach ($result as $fila){
-              
-                $activeSheet->setCellValue('A'.$row, $date);
-                $activeSheet->setCellValue('B'.$row, $fila["Estado"]);
-                $activeSheet->setCellValue('C'.$row, $fila["Municipio"]);
-                $activeSheet->setCellValue('D'.$row, $fila["Parroquia"]);
-                $activeSheet->setCellValue('E'.$row, $fila["Eje"]);
-                $activeSheet->setCellValue('F'.$row, $fila["Codigo"]);
-                $activeSheet->setCellValue('G'.$row, $fila["Centro"]);
-                $activeSheet->setCellValue('H'.$row, $fila["Cedula"]);
-                $activeSheet->setCellValue('I'.$row, $fila["Nombre"]);
-                $activeSheet->setCellValue('J'.$row, $fila["Telefono"]);
-                $activeSheet->setCellValue('K'.$row, $fila["Asistencia"]);
-                $activeSheet->setCellValue('L'.$row, $fila["Observaciones"]);
-                $activeSheet->setCellValue('M'.$row, $fila["StatusAct"]);
-                $activeSheet->setCellValue('N'.$row, $fila["ObservacionesStatus"]);
+
+
+            $activeSheet = $objPHPExcel->getActiveSheet();    
+
+            $activeSheet->setCellValue('K2', $date);
+            $row = 4; //Fila Inicial del skeleton
+
+            foreach ($result as $fila) {
+
+                $activeSheet->setCellValue('A' . $row, $date);
+                $activeSheet->setCellValue('B' . $row, $fila["Estado"]);
+                $activeSheet->setCellValue('C' . $row, $fila["Municipio"]);
+                $activeSheet->setCellValue('D' . $row, $fila["Parroquia"]);
+                $activeSheet->setCellValue('E' . $row, $fila["Eje"]);
+                $activeSheet->setCellValue('F' . $row, $fila["Codigo"]);
+                $activeSheet->setCellValue('G' . $row, $fila["Centro"]);
+                $activeSheet->setCellValue('H' . $row, $fila["Cedula"]);
+                $activeSheet->setCellValue('I' . $row, $fila["Nombre"]);
+                $activeSheet->setCellValue('J' . $row, $fila["Telefono"]);
+                $activeSheet->setCellValue('K' . $row, $fila["Asistencia"]);
+                $activeSheet->setCellValue('L' . $row, $fila["Observaciones"]);
+                $activeSheet->setCellValue('M' . $row, $fila["StatusAct"]);
+                $activeSheet->setCellValue('N' . $row, $fila["ObservacionesStatus"]);
                 $row++;
             }
-            
-            $fileName = sprintf('SIP - Asistencia del día -%s.xlsx', $now->format('Ymd-His'));
+
+            $fileName = sprintf('SIP - Asistencia de Cutls %s.xlsx', str_replace("/", "-", $date));
 
             header('Content-Type: application/vnd.ms-excel');
             header('Content-Disposition: attachment;filename="' . $fileName . '"');
