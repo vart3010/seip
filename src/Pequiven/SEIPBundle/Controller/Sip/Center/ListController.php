1d4d4b4efc8efc0bcee8daea461ea29b84c6e817
Gilberto <glavrjk@gmail.com>
2015-12-04 16:28:14 -0430

    Tablas Centro

--- src/Pequiven/SEIPBundle/Controller/Sip/Center/ListController.php
+++ src/Pequiven/SEIPBundle/Controller/Sip/Center/ListController.php
@@ -22,11 +22,43 @@ class ListController extends SEIPController {
         $em = $this->getDoctrine()->getManager();
         $em->getConnection()->beginTransaction();
 
-        $data = $this->get('pequiven.repository.siplist')->getGeneralVote();
+        $codigos = $this->get('pequiven.repository.siplist')->getCodes($request->get("edo"), $request->get("mcpo"), $request->get("parroq"));
+
+        if (($request->get("type")) == 1) {
+            $tipo = "General";
+        }
+
+        if (($request->get("type")) == 2) {
+            $tipo = "PQV";
+        }
+
+        if (($request->get("type")) == 3) {
+            $tipo = "1x10";
+        }
+
+        if (($request->get("type")) == 3) {
+            $tipo = "C5";
+        }
+
+        $nombres[0] = $codigos["DescriptionEstado"];
+        $nombres[1] = $codigos["DescriptionMunicipio"];
+        $nombres[2] = $codigos["DescriptionParroquia"];
+        $nombres[3] = $tipo;
+
+        $codigos[0] = $request->get("edo");
+        $codigos[1] = $request->get("mcpo");
+        $codigos[2] = $request->get("parroq");
+        $codigos[3] = $request->get("type");
+        $codigos[4] = $request->get("codigoCentro");
+
+
+        $data = $this->get('pequiven.repository.siplist')->getGeneralVote($nombres[0], $nombres[1], $nombres[2], $tipo, $codigos[4]);
 
         return $this->render('PequivenSEIPBundle:Sip:Center/List/ListGeneralVote.html.twig', array(
                     'user' => $this->getUser(),
-                    'data' => $data
+                    'data' => $data,
+                    'nombres' => $nombres,
+                    'codigos' => $codigos
         ));
     }
 
--- src/Pequiven/SEIPBundle/Repository/Sip/Centro/ListRepository.php
+++ src/Pequiven/SEIPBundle/Repository/Sip/Centro/ListRepository.php
@@ -11,27 +11,56 @@ use Pequiven\SEIPBundle\Doctrine\ORM\SeipEntityRepository as EntityRepository;
  */
 class ListRepository extends EntityRepository {
 
-    function getGeneralVote() {
+    function getGeneralVote($estado, $municipio, $parroquia, $tipo, $codigoCentro) {
 
         $em = $this->getEntityManager();
         $db = $em->getConnection();
 
-        $sql = 'SELECT 
-    Estado,
-    Municipio,
-    Parroquia,
-    IdCentro,
-    Codigo,
-    Centro,
-    SUM(VotoSI) AS Si,
-    SUM(VotoNO) AS No
-FROM
-    General_Votes
-WHERE
-    estado = "EDO. CARABOBO"
-        AND parroquia = "PQ. RAFAEL URDANETA"
-GROUP BY centro
-ORDER BY Codigo';
+        $sql1 = 'SELECT     Estado,    Municipio,    Parroquia,    IdCentro,    Codigo,    Centro,
+            SUM(VotoSI) AS Si,    SUM(VotoNO) AS No
+            FROM    General_Votes
+            WHERE
+            Estado = "' . $estado . '"
+            AND MUNICIPIO = "' . $municipio . '"
+            AND Parroquia = "' . $parroquia . '" ';
+
+        if ($tipo != "General") {
+            $sql2 = ' AND Tipo="' . $tipo . '" ';
+        } else {
+            $sql2 = ' ';
+        }
+
+        if (isset($codigoCentro)) {
+            $sql2 = ' AND CodigoCentro="' . $codigoCentro . '" ';
+        } else {
+            $sql2 = ' ';
+        }
+
+        $sql3 = ' GROUP BY centro ORDER BY Codigo';
+
+        $sql = $sql1 . $sql2 . $sql3;
+
+        $stmt = $db->prepare($sql);
+        $stmt->execute();
+        $result = $stmt->fetchAll();
+
+        return $result;
+    }
+
+    function getCodes($estado, $municipio, $parroquia) {
+
+        $em = $this->getEntityManager();
+        $db = $em->getConnection();
+
+        $sql = 'SELECT DISTINCT
+    codigoEstado,    descriptionEstado,    
+    codigoMunicipio,    descriptionMunicipio,    
+    codigoParroquia,    descriptionParroquia
+        FROM    sip_centro
+            WHERE
+            Estado = "' . $estado . '"
+            AND MUNICIPIO = "' . $municipio . '"
+            AND Parroquia = "' . $parroquia . '" ';
 
         $stmt = $db->prepare($sql);
         $stmt->execute();
