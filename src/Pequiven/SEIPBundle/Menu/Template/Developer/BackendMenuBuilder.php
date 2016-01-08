165a8073914562e08cb3a624eb56147a77b1c82f
Matias <matei249@gmail.com>
2016-01-08 15:15:42 -0430

    Cambio permiso

--- src/Pequiven/SEIPBundle/Menu/Template/Developer/BackendMenuBuilder.php
+++ src/Pequiven/SEIPBundle/Menu/Template/Developer/BackendMenuBuilder.php
@@ -1626,7 +1626,7 @@ class BackendMenuBuilder extends MenuBuilder implements \Symfony\Component\Depen
                 )->setLabel($this->translate(sprintf('app.backend.menu.%s.sip.main', $section)));
 
         
-        if($this->isGranted(array('ROLE_SEIP_SIP_SEACH_EMPLOYEES')) && ($user->getId() == 112 || $user->getId() == 22)){
+        if($this->isGranted(array('ROLE_SEIP_SIP_SEACH_EMPLOYEES')) && ($user->getId() == 112 || $user->getId() == 22 || $user->getId() == 1668)){
             $menuSip->addChild('sip.list_pqv', array(
                 'route' => 'pequiven_onePerTen_list',
                 'labelAttributes' => array('icon' => 'fa fa-table',)
