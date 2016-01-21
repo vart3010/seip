19778e6a1dd300e08bab2cb2b689aab71e088dad
Matias <matei249@gmail.com>
2016-01-21 13:19:30 -0430

    Bloqueo o Desbloqueo de los usuarios por el planning user view

--- src/Pequiven/SEIPBundle/Form/User.php
+++ src/Pequiven/SEIPBundle/Form/User.php
@@ -40,6 +40,7 @@ class User extends AbstractType {
             ->add('numPersonal',null,array('label' => 'pequiven_seip.numPersonal','translation_domain' => 'PequivenSEIPBundle'))
             ->add('complejo','entity',array('class' => 'Pequiven\MasterBundle\Entity\Complejo','property' => 'description','required' => false,'empty_data' => null,'empty_value' => 'Ninguna','label' => 'pequiven_seip.complejo','translation_domain' => 'PequivenSEIPBundle','attr' => array('class' => 'select2 input-xlarge')))
             ->add('gerencia','entity',array('class' => 'Pequiven\MasterBundle\Entity\Gerencia','property' => 'description','required' => false,'empty_data' => null,'empty_value' => 'Ninguna','label' => 'pequiven_seip.gerenciaFirst','translation_domain' => 'PequivenSEIPBundle','attr' => array('class' => 'select2 input-xlarge')))
+            ->add('locked','checkbox',array('label' => 'Bloqueado','required' => false))
             ->add('gerenciaSecond','entity',array('class' => 'Pequiven\MasterBundle\Entity\GerenciaSecond','property' => 'description','required' => false,'empty_data' => null,'empty_value' => 'Ninguna', 'required' => false ,'label' => 'pequiven_seip.gerenciaSecond','translation_domain' => 'PequivenSEIPBundle','attr' => array('class' => 'select2 input-xlarge')))
             ->add('direction',null,array('label' => 'pequiven_seip.direction','translation_domain' => 'PequivenSEIPBundle'))
             ->add('plantReports','entity',array(
