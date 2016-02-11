ab2d6fab8d7b1a877eb273acfc975b24d856e91b
Maximo <maxsojo13@gmail.com>
2016-02-11 17:58:39 -0430

    Desarrollo de navegacion en menu hacia lista de seguimiento y eficacia

--- src/Pequiven/SEIPBundle/Menu/Template/Developer/BackendMenuBuilder.php
+++ src/Pequiven/SEIPBundle/Menu/Template/Developer/BackendMenuBuilder.php
@@ -334,6 +334,22 @@ class BackendMenuBuilder extends MenuBuilder implements \Symfony\Component\Depen
             $menuSig->addChild($arrangementProgram);
             $menu->addChild($menuSig);
         }
+
+        $child = $this->factory->createItem('sig.monitoring.add.main', $this->getSubLevelOptions(array(
+                            'route' => null,
+                            'labelAttributes' => array('icon' => '',),
+                        ))
+                )
+                ->setLabel($this->translate(sprintf('app.backend.menu.%s.sig.monitoring.title', $section)));
+
+        $child->addChild('sig.monitoring.vizualice.main', array(
+                    'route' => 'pequiven_sig_monitoring_list',
+                    'labelAttributes' => array('icon' => ''),
+                ))
+                ->setLabel($this->translate(sprintf('app.backend.menu.%s.sig.monitoring.list', $section)));
+
+        $menuSig->addChild($child);       
+
     }
 
     /**
