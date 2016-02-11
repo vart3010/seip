5da10eb16088e2a71df306b833d9b01774b467f8
Maximo <maxsojo13@gmail.com>
2016-02-11 17:18:20 -0430

    Revision de Exportacion de informe de Seguimiento y Eficacia de las Acciones

--- src/Pequiven/SIGBundle/Controller/SigMonitoring/MonitoringController.php
+++ src/Pequiven/SIGBundle/Controller/SigMonitoring/MonitoringController.php
@@ -82,4 +82,69 @@ class MonitoringController extends ResourceController
     	
     	return $this->render('PequivenSIGBundle:Monitoring:show.html.twig', array('data' => $managemensystems));
     }
+
+    public function exportAction(Request $request){
+        $id = $request->get('id');
+        $managemensystems = $this->container->get('pequiven.repository.sig_management_system')->find($id); 
+        
+        $resource = $this->findOr404($request);
+        
+        //Formato para todo el documento
+        $styleArrayBordersContent = array(
+          'borders' => array(
+            'allborders' => array(
+              'style' => \PHPExcel_Style_Border::BORDER_THIN
+            )
+          ),
+          'font' => array(
+          ),
+          'alignment' => array(
+              'horizontal' => \PHPExcel_Style_Alignment::HORIZONTAL_JUSTIFY,
+              'vertical'   => \PHPExcel_Style_Alignment::VERTICAL_TOP,
+              'wrap' => true
+          )
+        );
+        
+        //Formato en negrita
+        $styleArray = array(
+            'font' => array(
+                'bold' => true
+            )
+        );
+        
+        $path = $this->get('kernel')->locateResource('@PequivenSIGBundle/Resources/skeleton/base-sig-se.xls');
+        $now = new \DateTime();
+        $objPHPExcel = \PHPExcel_IOFactory::load($path);
+        $objPHPExcel
+                ->getProperties()
+                ->setCreator("SEIP")
+                ->setTitle('SEIP - Seguimiento y Verificación de la Eficacia')
+                ->setCreated()
+                ->setLastModifiedBy('SEIP')
+                ->setModified()
+                ;
+        $objPHPExcel
+                ->setActiveSheetIndex(0);
+        $activeSheet = $objPHPExcel->getActiveSheet();
+        $activeSheet->getDefaultRowDimension()->setRowHeight();
+        
+        ///$fileName = sprintf('SEIP-Seguimiento y Verificación-%s-%s.xls',$now->format('Ymd-His'));
+        $fileName = 'SEIP-Seguimiento y Verificación-%s-%s.xls'.$now->format('Ymd-His');
+
+        header('Content-Type: application/vnd.ms-excel');
+        header('Content-Disposition: attachment;filename="'.$fileName.'"');
+        header('Cache-Control: max-age=0');
+        // If you're serving to IE 9, then the following may be needed
+        header('Cache-Control: max-age=1');
+
+        // If you're serving to IE over SSL, then the following may be needed
+        header ('Expires: Mon, 26 Jul 1997 05:00:00 GMT'); // Date in the past
+        header ('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT'); // always modified
+        header ('Cache-Control: cache, must-revalidate'); // HTTP/1.1
+        header ('Pragma: public'); // HTTP/1.0
+
+        $objWriter = \PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel5');
+        $objWriter->save('php://output');
+        exit;
+    }
 }
