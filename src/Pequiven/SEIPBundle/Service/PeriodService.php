03372e2067c1372f8baf0c515c6f3abe7d705581
Matias <matei249@gmail.com>
2016-02-15 07:56:43 -0430

    Eliminación de permisos de planificación en el período 2015

--- src/Pequiven/SEIPBundle/Service/PeriodService.php
+++ src/Pequiven/SEIPBundle/Service/PeriodService.php
@@ -193,10 +193,10 @@ class PeriodService extends ContainerAware {
             $result = true;
         }
 
-        $user = $this->getUser();
-        if ($user->getId() == 1668 || $user->getId() == 1798 || $user->getId() == 293 || $user->getId() == 5816 || $user->getId() == 110 || $user->getId() == 104 || $user->getId() == 295 || $user->getId() == 121) {
-            $result = true;
-        }
+//        $user = $this->getUser();
+//        if ($user->getId() == 1668 || $user->getId() == 1798 || $user->getId() == 293 || $user->getId() == 5816 || $user->getId() == 110 || $user->getId() == 104 || $user->getId() == 295 || $user->getId() == 121) {
+//            $result = true;
+//        }
 
         return $result;
     }
