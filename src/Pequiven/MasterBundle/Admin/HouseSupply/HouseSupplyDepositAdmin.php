7691d27b84c567e742fd7affc1e789b7e9f36042
Gilbert C <glavrjk@gmail.com>
2016-02-26 19:50:32 -0430

    Creacion de Maestros de Inventario de HouseSupply por Sonata

---
+++ src/Pequiven/MasterBundle/Admin/HouseSupply/HouseSupplyDepositAdmin.php
@@ -0,0 +1,60 @@
+<?php
+
+/*
+ * This file is part of the TecnoCreaciones package.
+ * 
+ * (c) www.tecnocreaciones.com
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Pequiven\MasterBundle\Admin\HouseSupply;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
+
+/**
+ * Administracion de Productos de HouseSupply
+ *
+ * @author Gilbert <glavrjk@gmail.com>
+ */
+class HouseSupplyDepositAdmin extends Admin {
+
+    protected function configureShowFields(ShowMapper $show) {
+        $show
+                ->add('id')
+                ->add('description')
+                ->add('complejo')
+        ;
+    }
+
+    protected function configureFormFields(FormMapper $form) {
+        $form
+                ->add('id')
+                ->add('description')
+                ->add('complejo')
+        ;
+    }
+
+    protected function configureDatagridFilters(DatagridMapper $filter) {
+        $filter
+                ->add('id')
+                ->add('description')
+                ->add('complejo')
+        ;
+    }
+
+    protected function configureListFields(ListMapper $list) {
+        $list
+                ->add('id')
+                ->add('description')
+                ->add('complejo')
+
+        ;
+    }
+
+}
---
+++ src/Pequiven/MasterBundle/Admin/HouseSupply/HouseSupplyProductAdmin.php
@@ -0,0 +1,72 @@
+<?php
+
+/*
+ * This file is part of the TecnoCreaciones package.
+ * 
+ * (c) www.tecnocreaciones.com
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Pequiven\MasterBundle\Admin\HouseSupply;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
+
+/**
+ * Administracion de Productos de HouseSupply
+ *
+ * @author Gilbert <glavrjk@gmail.com>
+ */
+class HouseSupplyProductAdmin extends Admin {
+
+    protected function configureShowFields(ShowMapper $show) {
+        $show
+                ->add('id')
+                ->add('code')
+                ->add('description')
+                ->add('price')
+                ->add('cost')
+                ->add('maxPerUser')
+        ;
+    }
+
+    protected function configureFormFields(FormMapper $form) {
+        $form
+                ->add('id')
+                ->add('code')
+                ->add('description')
+                ->add('price')
+                ->add('cost')
+                ->add('maxPerUser')
+        ;
+    }
+
+    protected function configureDatagridFilters(DatagridMapper $filter) {
+        $filter
+                ->add('id')
+                ->add('code')
+                ->add('description')
+                ->add('price')
+                ->add('cost')
+                ->add('maxPerUser')
+        ;
+    }
+
+    protected function configureListFields(ListMapper $list) {
+        $list
+                ->add('id')
+                ->add('code')
+                ->add('description')
+                ->add('price')
+                ->add('cost')
+                ->add('maxPerUser')
+
+        ;
+    }
+
+}
