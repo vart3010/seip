eafa6fc3b6ab37c3a2311246187711f20dd3a259
Maximo <maxsojo13@gmail.com>
2016-03-08 09:28:54 -0430

    Implementando dinamismo a notificaciones

--- src/Pequiven/SEIPBundle/Resources/public/notify.js
+++ src/Pequiven/SEIPBundle/Resources/public/notify.js
@@ -4,7 +4,8 @@ var urlMessageDelete;
 var urlFavMessage;
 
 $("#reload").click(function(){
-    console.log('reload');
+    //console.log('reload');
+    getData(urlData);
     //getData();
     /*$.ajax({
         url: "",
@@ -13,7 +14,7 @@ $("#reload").click(function(){
             $(this).html(s);
         }
     });*/
-    $('#notifyAll').load();
+    //$('#notifyAll').load();
 });
 
 function setNotifications(dataNotify){     
@@ -29,6 +30,11 @@ function setNotifications(dataNotify){
     $('#evolution').text(dataNotify['evolution']);            
 }
 
+function setUrlData(urlGetData){
+    urlData = urlGetData;
+    getData(urlData);
+}
+
 function setUrlMessage(urlGetMessage){
     urlMessage = urlGetMessage;
 }
@@ -41,7 +47,7 @@ function setUrlMessageFav(urlFav){
     urlFavMessage = urlFav;
 }
 
-function getData(urlData){
+function getData(urlData){    
     var data = {
         id: 1,                    
     }; 
@@ -81,12 +87,11 @@ function showMessage(id){
                 $('#new-message_' + id).css('display','none');                        
                 $('#out-message_' + id).css('display','block');
                 $('#buttonToll').css('display', 'block');                        
-                document.getElementById("messageNone").innerHTML = data['description'];
+                $('#messageNone').html(data['description']);
                 //document.getElementById("messageNone").innerHTML = "<a href=\"{{ path('pequiven_sig_monitoring_show',{'id': 1 })}}\">Ver</a>";
-                var path = data['path'];
-                //console.log(path);
-                document.getElementById("href").innerHTML = "<a href='{{ path('pequiven_sig_monitoring_show',{'id': 1 })}}' class='button red-gradient float-right with-tooltip' title='Visualizar'>Visualizar</a>";
-                document.getElementById("sectionButton").innerHTML = '<a href class="button" title="Eliminar" onclick="deleteMessage('+id+');"><span class="icon-trash"></span></a><a href class="button" title="Marcar como Importante" onclick="favouriteMessage('+id+');"><span class="icon-flag"></span></a>';                                                
+                var path = data['path'];                
+                $('#href').html("<a href='{{ path('pequiven_sig_monitoring_show',{'id': 1 })}}' class='button red-gradient float-right with-tooltip' title='Visualizar'>Visualizar</a>");
+                $('#sectionButton').html('<a href class="button" title="Eliminar" onclick="deleteMessage('+id+');"><span class="icon-trash"></span></a><a href class="button" title="Marcar como Importante" onclick="favouriteMessage('+id+');"><span class="icon-flag"></span></a>');
             }
     });
 }   
@@ -110,7 +115,8 @@ function deleteMessage(id){
                 $(location).attr('href', data["url"]);                                                
                 $('#iMessage_' + id).css('display','none');  
                 $('#buttonToll').css('display', 'none');                       
-                document.getElementById("messageNone").innerHTML = '<i class="fa fa-check"></i> Notificación Eliminada Satisfactoriamente';
+                $('#messageNone').html('<i class="fa fa-check"></i> Notificación Eliminada Satisfactoriamente');
+                getData(urlData);
             }
     });
 } 
@@ -134,19 +140,22 @@ function favouriteMessage(id){
                 $(location).attr('href', data["url"]);                                                
                 $('#iMessage_' + id).css('display','none');  
     			$('#buttonToll').css('display', 'none');                       
-                document.getElementById("messageNone").innerHTML = '<i class="fa fa-check"></i> Notificación enviada a Importantes';
+                $('#messageNone').html('<i class="fa fa-check"></i> Notificación enviada a Importantes');
+                getData(urlData);
             }
     });
 }   
 
 $("#notify").click(function(){
-    console.log('Notify');
+    console.log('Notify'); 
 }); 
 
 $("#fav").click(function(){
-    console.log('Favoritos');
+    //console.log('Favoritos');
+    $('#messageNone').text('Sección importantes, selecciones un mensaje.');
 });	
 
 $("#trash").click(function(){
-    console.log('Trash');
+    //console.log('Trash');
+    $('#messageNone').text('Sección eliminados, selecciones un mensaje.');    
 }); 
