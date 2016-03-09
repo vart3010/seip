9d9052dfe0dabe1a3adc52244cc9cac6c6308e5c
Victor Tortolero <vart10.30@gmail.com>
2016-03-09 14:06:52 -0430

    Arreglo en Error 5300 en WorkStudyCircle

--- src/Pequiven/SEIPBundle/Controller/Politic/WorkStudyCircleController.php
+++ src/Pequiven/SEIPBundle/Controller/Politic/WorkStudyCircleController.php
@@ -426,7 +426,7 @@ class WorkStudyCircleController extends SEIPController {
         $circle = $this->get('pequiven.repository.work_study_circle')->findAll(); //Carga los Criculos
 
         $criteria['phase'] = $phase;
-        $criteria['period'] = $this->getPeriodService()->getPeriodActive()->getId();
+        //$criteria['period'] = $this->getPeriodService()->getPeriodActive()->getId();
 
 
         if ($this->config->isPaginated()) {
