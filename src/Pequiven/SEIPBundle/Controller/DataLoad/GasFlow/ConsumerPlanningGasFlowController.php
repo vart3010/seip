c0707368a57547f72b1ec9894c3bbd6b654c1a3a
Matias <matei249@gmail.com>
2016-04-21 16:54:18 -0430

    Flujo de gas

---
+++ src/Pequiven/SEIPBundle/Controller/DataLoad/GasFlow/ConsumerPlanningGasFlowController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Pequiven\SEIPBundle\Controller\DataLoad\GasFlow;
+
+use Pequiven\SEIPBundle\Controller\SEIPController;
+
+/**
+ * Controlador de consumo de flujo de gas
+ *
+ * @author Matías Jiménez <matei249@gmail.com>
+ */
+class ConsumerPlanningGasFlowController extends SEIPController 
+{
+    public function createNew() {
+        $entity = parent::createNew();
+        $request = $this->getRequest();
+        $plantReportId = $request->get("plantReport");
+        if($plantReportId > 0){
+            $em = $this->getDoctrine()->getManager();
+            $plantReport = $em->find("Pequiven\SEIPBundle\Entity\DataLoad\PlantReport", $plantReportId);
+            $entity->setPlantReport($plantReport);
+        }
+        return $entity;
+    }
+    
+    public function deleteAction(\Symfony\Component\HttpFoundation\Request $request) 
+    {
+        $resource = $this->findOr404($request);
+        
+        $url = $this->generateUrl("pequiven_plant_report_show",array(
+            "id" => $resource->getPlantReport()->getId(),
+        ));
+        
+        $this->domainManager->delete($resource);
+        return $this->redirect($url);
+    }
+}
