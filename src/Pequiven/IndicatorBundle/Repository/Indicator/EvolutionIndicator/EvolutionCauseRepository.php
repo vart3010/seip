eaeabfd3cf94f0047378405a2e614d1d2561be25
Maximo <maxsojo13@gmail.com>
2016-07-13 16:10:13 -0430

    revision

--- src/Pequiven/IndicatorBundle/Repository/Indicator/EvolutionIndicator/EvolutionCauseRepository.php
+++ src/Pequiven/IndicatorBundle/Repository/Indicator/EvolutionIndicator/EvolutionCauseRepository.php
@@ -28,6 +28,37 @@ class EvolutionCauseRepository extends SeipEntityRepository {
         return $queryBuilder;
     }
 
+    /**
+     * Retornar las causas consultadas
+     * @return type
+     */
+    function findToValidToCausesEvolution(array $causes = array(), array $criteria = array()) {
+        return $this->findQueryToCauses($causes, $criteria)->getQuery()->getResult();
+    }
+
+    /**
+     *
+     *
+     */
+    public function findQueryToCauses(array $causes, array $criteria = array()){        
+        $criteria = new \Doctrine\Common\Collections\ArrayCollection($criteria);        
+        $qb = $this->getQueryBuilder();
+
+        $orX = $qb->expr()->orX();        
+        if (($cause = $criteria->remove('causes'))) {            
+            $orX->add($qb->expr()->like('ec.causes', "'%" . $cause . "%'"));
+        }
+        if (($id = $criteria->remove('id'))) {            
+            $orX->add($qb->expr()->like('ec.id', "'%" . $id . "%'"));
+        }
+
+        $qb->andWhere($orX);
+
+        $qb->setMaxResults(30);        
+        
+        return $qb;
+    }
+
     protected function getAlias() {
         return 'ec';
     }
