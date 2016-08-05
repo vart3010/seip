ae5925ceb3ea64b5b6d7626c5354c37767c4708f
Johana Rivas <johanarivas57@gmail.com>
2016-08-05 15:37:35 -0430

    cerrando IItrimestre pg sala oriente

--- src/Pequiven/SEIPBundle/EventListener/SerializerListener.php
+++ src/Pequiven/SEIPBundle/EventListener/SerializerListener.php
@@ -416,9 +416,9 @@ class SerializerListener implements EventSubscriberInterface, ContainerAwareInte
 //                $data['januaryReal']['isEnabled'] = true;
 //                $data['februaryReal']['isEnabled'] = true;
 //                $data['marchReal']['isEnabled'] = true;
-                $data['aprilReal']['isEnabled'] = true;
-                $data['mayReal']['isEnabled'] = true;
-                $data['juneReal']['isEnabled'] = true;
+                $data['aprilReal']['isEnabled'] = false;
+                $data['mayReal']['isEnabled'] = false;
+                $data['juneReal']['isEnabled'] = false;
                 //$data['julyReal']['isEnabled'] = true;
                 //$data['augustReal']['isEnabled'] = true;
                 //$data['septemberReal']['isEnabled'] = true;
