fa9e51daf6c6d70dc4408b889ac8e749a94fb01a
Gilbert C <glavrjk@gmail.com>
2016-08-18 10:25:55 -0430

    Arreglo de Bugs en signos de Ordenes Devueltas

--- src/Pequiven/SEIPBundle/Controller/HouseSupply/HouseSupplyOrderKitController.php
+++ src/Pequiven/SEIPBundle/Controller/HouseSupply/HouseSupplyOrderKitController.php
@@ -15,6 +15,7 @@ use Pequiven\SEIPBundle\Entity\HouseSupply\Order\houseSupplyOrder;
 use Pequiven\SEIPBundle\Entity\HouseSupply\Order\houseSupplyPayments;
 use Pequiven\SEIPBundle\Entity\HouseSupply\Inventory\houseSupplyInventoryCharge;
 use Pequiven\SEIPBundle\Entity\HouseSupply\Inventory\houseSupplyInventoryChargeItems;
+
 //use Pequiven\SEIPBundle\Entity\HouseSupply\Order\houseSupplyOrder;
 
 /**
@@ -89,16 +90,16 @@ class HouseSupplyOrderKitController extends SEIPController {
         $repository = $this->get('pequiven.repository.housesupply_order');
 //var_dump(get_class($repository));die();
 //$orders = $this->get('pequiven.repository.housesupply_order')->findAll(); //Carga las Órdenes
-        
+
         $arrayStatusHouseSupplyOrder = houseSupplyOrder::getStatus();
-        
+
         $statusHouseSupplyOrder = array();
-        $statusHouseSupplyOrder[] = array('id' => houseSupplyOrder::REGISTRADA,'description' => $arrayStatusHouseSupplyOrder[houseSupplyOrder::REGISTRADA]);
-        $statusHouseSupplyOrder[] = array('id' => houseSupplyOrder::DEVUELTA,'description' => $arrayStatusHouseSupplyOrder[houseSupplyOrder::DEVUELTA]);
-        $statusHouseSupplyOrder[] = array('id' => houseSupplyOrder::ESPERA,'description' => $arrayStatusHouseSupplyOrder[houseSupplyOrder::ESPERA]);
-        $statusHouseSupplyOrder[] = array('id' => houseSupplyOrder::PAGADA,'description' => $arrayStatusHouseSupplyOrder[houseSupplyOrder::PAGADA]);
-        $statusHouseSupplyOrder[] = array('id' => houseSupplyOrder::ENTREGADA,'description' => $arrayStatusHouseSupplyOrder[houseSupplyOrder::ENTREGADA]);
-        
+        $statusHouseSupplyOrder[] = array('id' => houseSupplyOrder::REGISTRADA, 'description' => $arrayStatusHouseSupplyOrder[houseSupplyOrder::REGISTRADA]);
+        $statusHouseSupplyOrder[] = array('id' => houseSupplyOrder::DEVUELTA, 'description' => $arrayStatusHouseSupplyOrder[houseSupplyOrder::DEVUELTA]);
+        $statusHouseSupplyOrder[] = array('id' => houseSupplyOrder::ESPERA, 'description' => $arrayStatusHouseSupplyOrder[houseSupplyOrder::ESPERA]);
+        $statusHouseSupplyOrder[] = array('id' => houseSupplyOrder::PAGADA, 'description' => $arrayStatusHouseSupplyOrder[houseSupplyOrder::PAGADA]);
+        $statusHouseSupplyOrder[] = array('id' => houseSupplyOrder::ENTREGADA, 'description' => $arrayStatusHouseSupplyOrder[houseSupplyOrder::ENTREGADA]);
+
         $securityService = $this->getSecurityService();
         if (!$securityService->isGranted(array("ROLE_SEIP_HOUSESUPPLY_VIEW_ALL_ORDERS"))) {
             $criteria['ownWsc'] = $this->getUser()->getWorkStudyCircle()->getId();
@@ -732,12 +733,14 @@ class HouseSupplyOrderKitController extends SEIPController {
 
         foreach ($order->getOrderItems() as $orderItems) {
             $orderItems->setType(2);
+            $orderItems->setSign(-1);
             $orderItems->setDeletedAt($date);
             $orderItems->setDeletedBy($this->getUser());
             $em->flush();
         }
 
         $order->setType(2);
+        $order->setSign(-1);
         $order->setDeletedAt($date);
         $order->setDeletedBy($this->getUser());
         $em->flush();
