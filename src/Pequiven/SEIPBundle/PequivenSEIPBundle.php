e1fda63a9527206bf4cf1cb6bfab05a4f6acdf62
Matias Jiménez <matei249@gmail.com>
2016-08-29 10:36:48 -0430

    Texto de versión en el footer

--- src/Pequiven/SEIPBundle/PequivenSEIPBundle.php
+++ src/Pequiven/SEIPBundle/PequivenSEIPBundle.php
@@ -7,6 +7,6 @@ use Pequiven\CoreBundle\PequivenCoreBundle;
 
 class PequivenSEIPBundle extends Bundle
 {
-    const VERSION = '2.0.6';
-    const VERSION_DATE = 'Desde el 22-08-2016 07:00am';
+    const VERSION = '2.0.7';
+    const VERSION_DATE = 'Desde el 29-08-2016 07:00am';
 }
